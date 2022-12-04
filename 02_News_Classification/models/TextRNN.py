55e477fa4ec5de3ab3cec53d6c811bec6927adfe
Reid-Cui7 <reid.cmj@gmail.com>
2022-12-04 17:24:20 +0800

    finish TextRNN.py

--- 02_News_Classification/models/TextRNN.py
+++ 02_News_Classification/models/TextRNN.py
@@ -31,6 +31,21 @@ class Config:
         self.hidden_size = 128 # LSTM隐藏层
         self.num_layers = 2
 
+
 class Model(nn.Module):
     def __init__(self, config):
-        ...
+        super(Model, self).__init__()
+        if config.embedding_pretrained:
+            self.embedding = nn.Embedding.from_pretrained(config.embedding_pretrained, freeze=False)
+        else:
+            self.embedding = nn.Embedding(config.n_vocab, config.embed, padding_idx=config.n_vocab-1)
+
+        self.lstm = nn.LSTM(config.embed, config.hidden_size, config.num_layers, bidirectional=True, batch_first=True, dropout=config.dropout)
+        self.fc = nn.Linear(config.hidden_size * 2, config.num_classes)
+
+    def forward(self, x):
+        x, _ = x
+        out = self.embedding(x) # [batch_size, seq_len, embedding] [128, 32, 300]
+        out, _ = self.lstm(out)
+        out = self.fc(out[:, -1, :]) # 句子最后时刻的hidden state
+        return out

76ef3e3fc977ecd6b29eeb260e0d47eabc1938f4
Reid-Cui7 <reid.cmj@gmail.com>
2022-12-04 17:14:30 +0800

    finish utils.py

--- 02_News_Classification/run.py
+++ 02_News_Classification/run.py
@@ -31,3 +31,21 @@ if __name__ == "__main__":
     x = import_module("models." + model_name)
     config = x.Config(dataset, embedding)
 
+    # 设定随机种子
+    np.random.seed(7)
+    torch.manual_seed(7)
+    torch.cuda.manual_seed_all(7)
+    torch.backends.cudnn.deterministic = True
+
+    start_time = time.time()
+    print("Loading data...")
+    vocab, train_data, dev_data, test_data = build_dataset(config, args.word)
+    train_iter = build_iterator(train_data, config)
+    dev_iter = build_iterator(dev_data, config)
+    test_iter = build_iterator(test_data, config)
+    time_dif = get_time_dif(start_time)
+    print("Time usage:", time_dif)
+
+
+
+
